e22431a0dc01ee283e06bc8f1aa23552b8e48f64
281985929 <281985929+kasimali-hmrc@users.noreply.github.com>
2026-08-19 15:19:13 +0100

    Used NinoPrefixGenerator for ICB bruno tests

--- ICB Bruno Tests/_lib/helpers.js
+++ ICB Bruno Tests/_lib/helpers.js
@@ -12,6 +12,29 @@ const randomInvalidNino = () => {
   return `ZZ${number}`;
 }
 
+const ninoPrefixGenerator = () => {
+  const ValidChar1 = 'ABCEGHJKLMNOPRSTWXYZ'.split(''); // Excludes DFIQUV
+  const ValidChar2 = 'ABCEGHJKLMNPRSTWXYZ'.split(''); // Excludes DFIOQUV
+  const InvalidPrefixes = new Set(['BG', 'GB', 'KN', 'NK', 'NT', 'TN', 'ZZ']);
+
+  const getValidPrefix = () => {
+    let prefix;
+    do {
+      const c1 = ValidChar1[Math.floor(Math.random() * ValidChar1.length)];
+      const c2 = ValidChar2[Math.floor(Math.random() * ValidChar2.length)];
+      prefix = c1 + c2;
+    } while (InvalidPrefixes.has(prefix));
+
+    return prefix;
+  };
+
+  const prefix = getValidPrefix();
+  const digits = Array.from({ length: 3 }, () => Math.floor(Math.random() * 10)).join('');
+
+  return prefix + digits;
+};
+
+
 const randomGovUkOriginatorId = () => {
   const chars = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789';
   const length = Math.floor(Math.random() * 38) + 3; // 3 to 40 characters long
@@ -33,5 +56,6 @@ module.exports = {
   randomInvalidNino,
   randomGovUkOriginatorId,
   randomUniversalCreditRecordType,
-  randomUniversalCreditAction
+  randomUniversalCreditAction,
+  ninoPrefixGenerator
 };
