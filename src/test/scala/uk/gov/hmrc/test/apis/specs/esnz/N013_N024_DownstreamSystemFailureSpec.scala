6bee4bac0975a1a11fbda9c1811bb0a4ee492a05
281985929 <281985929+kasimali-hmrc@users.noreply.github.com>
2026-08-19 10:27:02 +0100

    Fixed an issue with an incorrect nino

--- src/test/scala/uk/gov/hmrc/test/apis/specs/esnz/N013_N024_DownstreamSystemFailureSpec.scala
+++ src/test/scala/uk/gov/hmrc/test/apis/specs/esnz/N013_N024_DownstreamSystemFailureSpec.scala
@@ -1,3 +1,19 @@
+/*
+ * Copyright 2026 HM Revenue & Customs
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
 package uk.gov.hmrc.test.apis.specs.esnz
 
 import org.scalatest.BeforeAndAfterEach
@@ -22,7 +38,7 @@ class N013_N024_DownstreamSystemFailureSpec extends BaseSpec with CommonSteps wi
         "Michael",
         "Johnson",
         "1990-06-27",
-        "BB000001A",
+        "AA000035A",
         "2023-05-01",
         503
       ),
